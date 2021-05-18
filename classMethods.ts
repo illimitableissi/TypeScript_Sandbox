db4bda5e7e63104ef41a2423db8dd8029eb05767
SOVEREIGN <isimemeni@gmail.com>
2021-05-17 23:01:17 -0400

    working through class methods in typescript

---
+++ classMethods.ts
@@ -0,0 +1,30 @@
+class Department {
+    name: string;
+    employees: string[] = [];
+
+    constructor(n: string){
+        this.name = n;
+    }
+
+    describe(this: Department) {
+        console.log('Department: ' + this.name)
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees)
+    }
+}
+
+const accounting = new Department("Accounting");
+
+accounting.addEmployee("issi");
+accounting.addEmployee("Max");
+accounting.printEmployeeInformation();
+
+
+// accounting.describe()
