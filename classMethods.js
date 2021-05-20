60e2f22c0b025e2afb98fa64056736d602e4098f
SOVEREIGN <isimemeni@gmail.com>
2021-05-19 22:34:24 -0400

    working through class methods in typescript

--- classMethods.js
+++ classMethods.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Department = /** @class */ (function () {
     //readonly keyword - can only be initialized once and cannot be changed
     function Department(id, name) {
@@ -5,7 +20,8 @@ var Department = /** @class */ (function () {
         this.name = name;
         //private id: string;
         //private name: string;
-        //employees is now only accessible inside the class
+        //employees is now only accessible inside the class using the private methof
+        //protected method allows employees to be changed by other classes that extend this class
         this.employees = [];
         // this.name = name;
     }
@@ -21,7 +37,42 @@ var Department = /** @class */ (function () {
     };
     return Department;
 }());
-var accounting = new Department("d1", "Accounting");
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    // admins: string[];
+    function ITDepartment(id, admins) {
+        var _this = 
+        //whenever you are making a new class that is inheriting another class you must add a super.
+        _super.call(this, id, 'IT') || this;
+        _this.admins = admins;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, 'Accounting') || this;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addEmployee = function (name) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    };
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var it = new ITDepartment("d1", ['Issi, Max']);
+var accounting = new AccountingDepartment("d1", []);
 accounting.addEmployee("issi");
 accounting.addEmployee("Max");
 accounting.printEmployeeInformation();
--- classMethods.ts
+++ classMethods.ts
@@ -1,8 +1,9 @@
 class Department {
     //private id: string;
     //private name: string;
-    //employees is now only accessible inside the class
-    private employees: string[] = [];
+    //employees is now only accessible inside the class using the private methof
+    //protected method allows employees to be changed by other classes that extend this class
+    protected employees: string[] = [];
 
     //readonly keyword - can only be initialized once and cannot be changed
     constructor(private readonly id: string, public name: string){
@@ -33,12 +34,29 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport
+        }
+        throw new Error('No report found.')
+    }
+
     constructor(id: string, private reports: string[]) {
         super(id, 'Accounting');
+        this.lastReport = reports[0];
+    }
+    addEmployee(name: string){
+        if(name === 'Max') {
+            return;
+        }
+        this.employees.push(name)
     }
 
     addReport(text: string){
         this.reports.push(text);
+        this.lastReport = text
     }
 
     printReports(){
@@ -48,6 +66,7 @@ class AccountingDepartment extends Department {
 
 const it = new ITDepartment("d1", ['Issi, Max']);
 const accounting = new AccountingDepartment("d1", []);
+console.log(accounting.mostRecentReport)
 
 accounting.addEmployee("issi");
 accounting.addEmployee("Max");
