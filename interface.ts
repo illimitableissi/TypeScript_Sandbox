0e4fdd60a79047a0c3e8427576ad2dfee9bcde95
SOVEREIGN <isimemeni@gmail.com>
2021-05-26 13:26:35 -0400

    working through intersections in typescript

--- interface.ts
+++ interface.ts
@@ -10,24 +10,42 @@ interface Greetable extends Named {
 }
 
 interface Named {
-    readonly name: string;
+    readonly name?: string;
+    // ? = means optional
+    outputName?: string;
 }
 
 class Person implements Greetable {
-    name: string;
+    name?: string;
+    // outputName: 'Knives'
     age = 30;
 
-    constructor (n:string) {
-        this.name = n;
+    constructor (n?:string) {
+        if (n) {
+            this.name = n;  
+        }      
     }
 
     greet(phrase: string){
+        if (this.name) {
         console.log(phrase)
+        }
+        else {
+            console.log("Hi")
+        }
     }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Issi')
+user1 = new Person();
 
 user1.greet('Hi there, im')
+
+interface AddFn {
+    (a: number, b:number): number;
+}
+// type AddFn = (a: number, b:number) => number
+let add: AddFn;
+
+add = (n1: number, n2: number) => n1+n2
---
+++ intersections.ts
@@ -0,0 +1,25 @@
+//intersection types can be used to combine different types
+
+
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: 'Issi',
+    privileges: ['create-server'],
+    startDate: new Date()
+}
+
+type Combinable = string | number;
+type Numberic = number | boolean;
+
+type Universal = Combinable & Numberic
