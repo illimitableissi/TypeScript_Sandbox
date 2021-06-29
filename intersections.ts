2bd6c3c72dc7103c9b215b8086d56028de9b3d74
SOVEREIGN <isimemeni@gmail.com>
2021-06-29 13:42:00 -0400

    working on generics  section

--- intersections.ts
+++ intersections.ts
@@ -23,12 +23,20 @@ type Numberic = number | boolean;
 
 type Universal = Combinable & Numberic
 
+//Function Overload
+function add(a: number, b:number): number;
+function add(a:string, b:string): string;
+function add(a: number, b:string): string;
+function add(a: string, b:number): string;
 function add(a: Combinable, b:Combinable) {
     if (typeof a === 'string' || typeof b === 'string'){
         return a.toString() + b.toString();
     } 
     return a + b
 }
+
+const result = add('Max', 'Schwarz');
+result.split(' ')
 
 type UnkownEmployee = Employee | Admin;
 
@@ -79,20 +87,68 @@ function useVehicle(vehicle: Vehicle) {
 useVehicle(v1)
 useVehicle(v2)
 
+//Discrimated Unions
+
 interface Bird {
-    
+    type: 'bird';
     flyingSpeed: number;
 }
 
 interface Horse {
+    type: 'horse';
     runningSpeed: number;
 }
 
 type Animal = Bird | Horse;
 
 function moveAnimal(animal: Animal){
-    if('flyingSpeed' in animal) {
-        console.log('Moving with speed: ' +animal.flyingSpeed)
-    }
+    let speed;
+   switch(animal.type) {
+       case 'bird':
+           speed = animal.flyingSpeed;
+           break
+        case 'horse':
+            speed = animal.runningSpeed
+       }
+       console.log('Moving at speed: ' + speed)
 }
+
+moveAnimal({type: 'bird', flyingSpeed: 10})
+
+//Type Casting
+
+// const userInputElement = <HTMLInputElement>document.getElementById('')
+const userInputElement = document.getElementById('')! as HTMLInputElement;
+
+if (userInputElement){
+    (userInputElement as HTMLInputElement).value = 'Hi there'
+}
+
+const paragraph = document.querySelector('p');
+
+interface ErrorContainer { // { email: 'Not a valid email, userName: 'Must start with a character}
+    //every property must be string and value must also be a string
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email: 'Not a valid email!',
+    userName: 'Must start with a valid character!'
+};
+
+//Optional Chaining 
+
+const fetchedUserData = {
+    id: 'ul',
+    name: 'Issi',
+    job: {title: 'CEO', desciption: 'My own company'}
+};
+
+// console.log(fetchedUserData.job && fetchedUserData.job.title)
+console.log(fetchedUserData?.job?.title);
+
+const userInput = null;
+
+//Nullish Coalescing = ?? (null or undefined)
+const storedData = userInput ?? 'DEFAULT'
 
